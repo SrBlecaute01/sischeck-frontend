352d39ef98ee0db7b90b357f4ab6fcd61c467b47
José <jose@arnaldo.dev>
2025-10-11 19:16:52 -0300

    fix: sisweek backend service enviroment

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = import.meta.env.SISCHECK_SERVICE_URL
+const baseUrl = import.meta.env.VITE_SISCHECK_SERVICE_URL
 
 const api = axios.create({
   baseURL: baseUrl
